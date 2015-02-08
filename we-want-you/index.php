20478df355cbe2771cf877e8ab206aeb452fcdb9
mrkvon <michal.salajka@email.cz>
2015-02-08 19:46:08 +0200

    added we-want-you = join development page

---
+++ we-want-you/index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 'if you want to contribute to development of this project, write a message to michal[dot]salajka[at]email[dot]cz. We can email, talk over the internet, meet in real world and we can do it together.';
+
+echo '<br />';
+
