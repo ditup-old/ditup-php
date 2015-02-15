1557b9429863faec0367e3c77a0ff752490e1f2d
mrkvon <michal.salajka@email.cz>
2015-02-15 15:59:13 +0200

    minor bug fixes - link in 404 & router

--- mvc/app/views/general/404.php
+++ mvc/app/views/general/404.php
@@ -9,7 +9,7 @@ $content = '
     <div>
         404 Page Not Found.</div>
     <div>
-    If this shouldn\'t happen (i.e. standard navigation through website), please, <a href="feedback">report</a> to the developers. Help this platform improve.
+    If this shouldn\'t happen (i.e. standard navigation through website), please, <a href="/feedback">report</a> to the developers. Help this platform improve.
     </div>
     <div>
     Thank you.
