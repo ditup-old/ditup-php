126bb033d63dadcdbcc9583f30adfce8630d5b02
mrkvon <michal.salajka@email.cz>
2015-03-10 16:55:49 +0200

    improving ajax search - showing simple results

--- mvc/public/js/ajax-search-header.js
+++ mvc/public/js/ajax-search-header.js
@@ -1,16 +1,23 @@
 (function($){
   var xhr;
   
-  (function(input, xhr){
+  (function(input, xhr, results){
     input.bind('keyup', function(e){
       if(xhr && xhr.readyState != 4){
         xhr.abort();
       }
       xhr = $.ajax('/ajax-search',{data: "szuk="+input.val(),type:'POST',async:true,success:function(backpack){
-        console.log(backpack);
+        backpack = JSON.parse(backpack);
+	results.empty();
+        for(var user in backpack.users){
+          $(document.createElement('div')).appendTo(results).text(backpack.users[user].username);
+	}
+        for(var dit in backpack.dits){
+          $(document.createElement('div')).appendTo(results).text(backpack.dits[dit].ditname);
+	}
       }});
     });
-  })($('#search-form-header>input'), xhr);
+  })($('#search-form-header>input'), xhr, $('#search-form-header-results'));
   
   
   
